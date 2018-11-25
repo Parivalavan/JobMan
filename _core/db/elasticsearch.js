413879b465d436d52e04b8e8d2216d53d0952e6a
Parivalavan Manoharan <parivalavanm@gmail.com>
2018-11-25 20:41:23 +0530

    initial commit

---
+++ _core/db/elasticsearch.js
@@ -0,0 +1,92 @@
+var esconfig = require(__dirname + '/configs/esconfig.json')
+var unirest = require("unirest");
+
+var es = module.exports = {};
+
+/**
+ * a generic function to do the gets|posts|etc
+ * @param {string} method - GET|POST|HEAD|etc
+ * @param {string} url - endpoint to call
+ * @param {object} headers - headers to be sent along with the request
+ * @param {object} data - data to be sent
+ */
+function interact(method, url, headers, data) {
+    return new Promise(function (resolve, reject) {
+        headers = headers ? headers : {
+            "cache-control": "no-cache",
+            "Content-Type": "application/x-www-form-urlencoded"
+        };
+        method = method ? method : 'GET';
+
+        var req = unirest(method, url);
+        req.headers(headers);
+
+        req.end(function (res) {
+            if (res.error) {
+                reject({
+                    status: { code: res.code, message: "failed" },
+                    message: { error: res.error }
+                });
+            }
+            else {
+                resolve({
+                    status: { code: 200, message: "success" },
+                    message: { response: res.body }
+                });
+            }
+        });
+    });
+}
+
+/**
+ * check if elasticsearch is alive
+ */
+es.checkConnection = function () {
+    return new Promise(function (resolve, reject) {
+        interact('GET', esconfig.url, {
+            "cache-control": "no-cache",
+            "Content-Type": "application/x-www-form-urlencoded"
+        })
+        .then(function (obj) {
+            resolve(obj)
+        })
+        .catch(function (obj) {
+            reject(obj);
+        });
+    });
+}
+
+/**
+ * check if index is present of the given name, if not try to create an index
+ * @param {string} indexName - name of the index to create
+ * @param {dataObj} dataObj - object schema
+ */
+es.checkTable = function(indexName, dataObj){
+    return new Promise(function (resolve, reject) {
+        interact('HEAD', esconfig.url + '/' + indexName, {
+            "cache-control": "no-cache",
+            "Content-Type": "application/x-www-form-urlencoded"
+        })
+        .then(function (obj) {
+            resolve(obj)
+        })
+        .catch(function (obj) {
+            // if the status code is 404 then index is not available, create one
+            if (obj.status.code == 404){
+                interact('PUT', esconfig.url + '/' + indexName, {
+                    "cache-control": "no-cache",
+                    "Content-Type": "application/x-www-form-urlencoded"
+                })
+                .then(function (obj) {
+                    resolve(obj)
+                })
+                .catch(function (obj) {
+                    reject(obj);
+                });
+            }
+            else {
+                reject(obj);
+            }
+        });
+    });
+}
---
+++ app.js
@@ -0,0 +1,91 @@
+global.config = {};
+
+var bodyParser = require('body-parser')
+    , compression = require('compression')
+    , config = require('./_config/config.json')
+	, express = require('express')
+	, favicon = require('serve-favicon')
+    , helmet = require('helmet')
+    , path = require('path')
+	, session = require('express-session')
+;
+
+global.config = config;
+
+// session options, with session time set to 30 minutes, resetting maxAge on every request made
+var sessionOptions = {
+	secret: config.sessionKey,
+	resave: false,
+	saveUninitialized: true,
+	cookie: { maxAge : 1800000 },
+    rolling: true
+};
+
+var app = express();				// call the express framwork
+app.use(express.static(__dirname + "/public"));
+// set the required headers
+app.use(helmet());
+// deliver compressed content
+app.use(compression());
+// set the sessions
+app.use(session(sessionOptions));
+// display favicon
+app.use(favicon(path.join(__dirname, 'images', 'favicons', 'favicon.ico')))
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+// parse application/json
+app.use(bodyParser.json({ type: 'application/json'}));
+
+// app exit normally
+process.on('exit', function () {
+	console.log('Shutting down JobMan');
+	process.exit();
+});
+
+// catch ctrl+c event and exit normally
+process.on('SIGINT', function () {
+	console.log('Force quitting JobMan');
+	process.exit();
+});
+
+//catch uncaught exceptions, trace, then exit normally
+process.on('uncaughtExceptio1n', function(e) {
+    console.log('Uncaught Exception...');
+    console.log(e.stack);
+	process.exit();
+});
+
+var db = require('./_core/db/' + config.dbType + '.js');
+
+db.checkConnection()
+.then(function(status){
+    return db.checkTable(config.tableName, config.dataObj);
+})
+.then(function(status){
+    console.log('db status', status)
+    appServer = require('http').createServer(app).listen(config.port, function() {
+        console.log('                                +-+-+-+-+-+-+-+-+-+-+-+-+');
+        console.log('                                |   J o b M a n   1.0   |');
+        console.log('                                +-+-+-+-+-+-+-+-+-+-+-+-+');
+        console.log('');
+        console.log('process environment    : ' + (process.env.NODE_ENV || 'development'));
+        console.log('listening on port      : ' + config.port);
+        console.log('JobMan is ready ...');
+		// Socket.io server listens to our app
+		var io = require('socket.io').listen(appServer);
+		// Emit welcome message on connection
+		io.on('connection', function(socket) {
+			// Use socket to communicate with this particular client only, sending it it's own id
+			socket.emit('welcome', { message: 'Welcome!', id: socket.id });
+		});
+		// app.use(expressStatus({ websocket: io, port: app.get('port') }));			// status monitor
+		app.use(expReqLogger());
+		routers = require('./_core/routers.js')(io);
+		app.use(routers); // call the controllers
+
+    });
+})
+.catch(function(e){
+	console.log('Unable to establish connection to database', e);
+	process.exit();
+});
